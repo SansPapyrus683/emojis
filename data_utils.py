2e5c2e088f409a4f371ee223a3a6b0735fe89b82
Kevin Sheng <kevinsheng2005@gmail.com>
2023-10-12 13:34:38 -0700

    this is actually so scuffed

--- data_utils.py
+++ data_utils.py
@@ -2,23 +2,22 @@ import os
 import pathlib
 from PIL import Image
 import torch
-import torchvision.transforms as transforms
+from torchvision import io, transforms
 
 
 class Dataset(torch.utils.data.Dataset):
     def __init__(self, image_folder_path: str, desired_image_size: tuple[int, int]):
-        transform = transforms.Compose([
-            transforms.Resize(desired_image_size),
-            transforms.ToTensor(),
-        ])
-
         # initialize the data from the path
         self.data = []
         start = pathlib.Path(image_folder_path)
+        resize = transforms.Resize(desired_image_size)
         for domain in os.listdir(start):
+            if domain in ["KDDI", "DoCoMo", "SoftBank", "Gmail"]:
+                continue
             for img in os.listdir(start / domain):
                 path = start / domain / img
-                self.data.append(transform(Image.open(start / domain / img)))
+                tensor = io.read_image(str(path))
+                self.data.append(resize(tensor))
 
     def __getitem__(self, i):
         # return the ith image as a tensor
@@ -31,4 +30,6 @@ class Dataset(torch.utils.data.Dataset):
 
 if __name__ == "__main__":
     d = Dataset("data/image", (64, 64))
-    print(d[0])
+    x = d[0]
+    print(x)
+    print(transforms.ToPILImage()(x))
